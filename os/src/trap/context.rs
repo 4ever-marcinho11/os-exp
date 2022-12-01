4dcb2b59ab126e7aeab742a824483c5857cade06
marcinho <maximilian-ivan@outlook.com>
2022-12-01 15:21:34 +0800

    20301111 liyifan 1.7 first complete

--- os/src/trap/context.rs
+++ os/src/trap/context.rs
@@ -1,24 +1,25 @@
-use riscv::register::sstatus::{Sstatus, self, SPP};
+use crate::trap::trap_return;
 
 #[repr(C)]
-pub struct TrapContext {
-    pub x: [usize; 32],
-    pub sstatus: Sstatus,
-    pub sepc: usize,
+pub struct TaskContext {
+    ra: usize,
+    sp: usize,
+    s: [usize; 12],
 }
 
-impl TrapContext {
-    pub fn set_sp(&mut self, sp: usize) { self.x[2] = sp; }
-    pub fn app_init_context(entry: usize, sp: usize) -> Self {
-        let mut sstatus = sstatus::read();
-        sstatus.set_spp(SPP::User);
-        let mut cx = Self {
-            x: [0; 32],
-            sstatus,
-            sepc: entry,
-        };
-        cx.set_sp(sp);
-        cx
+impl TaskContext {
+    pub fn zero_init() -> Self {
+        Self {
+            ra: 0,
+            sp: 0,
+            s: [0; 12],
+        }
+    }
+    pub fn goto_trap_return(kstack_ptr: usize) -> Self {
+        Self {
+            ra: trap_return as usize,
+            sp: kstack_ptr,
+            s: [0; 12],
+        }
     }
 }
-
