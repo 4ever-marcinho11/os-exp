16d78139c21aba9f2d685675d177113dbe6b6a41
marcinho <maximilian-ivan@outlook.com>
2022-12-11 09:40:54 +0800

    20301111 liyifan hotline

---
+++ user/src/bin/04my_own.rs
@@ -0,0 +1,22 @@
+#![no_std]
+#![no_main]
+
+#[macro_use]
+extern crate user_lib;
+
+#[no_mangle]
+fn main() -> i32 {
+    println!("*****************");
+    let mut arr = [5, -3, 0, 3, 1, 2, -1, 8, 7];
+    println!("before: {:?}",arr);
+    for i in 0..arr.len() {
+        for j in 0..arr.len() - 1 - i {
+            if arr[j] > arr[j + 1] {
+                arr.swap(j, j + 1);
+            }
+        }
+    }
+    println!("after：{:?}",arr);
+    println!("*****************");
+    0
+}
